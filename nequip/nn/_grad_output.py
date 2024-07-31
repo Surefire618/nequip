Fix bug from previous implementation (self.func() only called once with forward-mode jacobian)

nequip/nn/_grad_output.py
@@ -362,7 +362,7 @@ class StressOutput(GraphModuleMixin, torch.nn.Module):
 
         return data
 
-@compile_mode("script")
+@compile_mode("unsupported")
 class StrainStressOutput(GraphModuleMixin, torch.nn.Module):
     r"""Compute stress (and forces) using autograd from strain.
 
@@ -445,6 +445,25 @@ class StrainStressOutput(GraphModuleMixin, torch.nn.Module):
         did_pos_req_grad: bool = pos.requires_grad
         pos.requires_grad_(True)
 
+        def all_wrapper(strain: torch.Tensor) -> torch.Tensor:
+            nonlocal data
+            # bmm is natom in batch
+            # batched [natom, 1, 3] @ [natom, 3, 3] -> [natom, 1, 3] -> [natom, 3]
+            data[AtomicDataDict.POSITIONS_KEY] = pos + torch.bmm(
+                pos.unsqueeze(-2), torch.index_select(strain, 0, batch)
+            ).squeeze(-2)
+            # assert torch.equal(pos, data[AtomicDataDict.POSITIONS_KEY])
+            # we only displace the cell if we have one:
+            if has_cell:
+                # [n_batch, 3, 3] @ [n_batch, 3, 3]
+                data[AtomicDataDict.CELL_KEY] = cell + torch.bmm(
+                    cell, strain
+                )
+
+            # Call model and get gradients
+            data = self.func(data)
+            return data[AtomicDataDict.PER_ATOM_ENERGY_KEY].squeeze(-1)
+
         def batch_wrapper(strain: torch.Tensor, n_batch: int, batch_idx: list[int]) -> torch.Tensor:
             nonlocal data
             _strain = torch.zeros(
@@ -454,6 +473,7 @@ class StrainStressOutput(GraphModuleMixin, torch.nn.Module):
             )
             _strain[n_batch,:,:] = strain
             _strain.requires_grad_(True)
+            pos.requires_grad_(True)
             # bmm is natom in batch
             # batched [natom, 1, 3] @ [natom, 3, 3] -> [natom, 1, 3] -> [natom, 3]
             data[AtomicDataDict.POSITIONS_KEY] = pos + torch.bmm(
@@ -496,33 +516,59 @@ class StrainStressOutput(GraphModuleMixin, torch.nn.Module):
         num_atoms = pos.shape[0] # batched number of atoms
         atomic_virial = torch.zeros(
             (num_atoms, 3, 3),
-            dtype=torch.float64, # FIXME 
+            dtype=torch.float64, # FIXME
+            # dtype=pos.dtype, # FIXME
             device=pos.device,
         )
         if num_batch > 1:
+            # print(f"{num_batch = }")
+            jac = torch.autograd.functional.jacobian(
+                func=all_wrapper,
+                inputs=data["_strain"],
+                # create_graph=self.training,  # needed to allow gradients of this output during training
+                vectorize=self.vectorize,
+                strategy="forward-mode",
+            )
+            # print(f"{jac.shape = }")
+            # print(f"{jac = }")
             for n_batch in range(num_batch):
                 batch_i = data[AtomicDataDict.BATCH_PTR_KEY][n_batch]
                 batch_j = data[AtomicDataDict.BATCH_PTR_KEY][n_batch+1]
                 batch_idx = list(range(batch_i, batch_j))
-                grad = torch.autograd.functional.jacobian(
-                    func=lambda s: batch_wrapper(s, n_batch, batch_idx),
-                    inputs=data["_strain"][n_batch, :, :],
-                    # create_graph=self.training,  # needed to allow gradients of this output during training
-                    vectorize=self.vectorize,
-                    strategy="forward-mode",
-                )
-                atomic_virial[batch_idx, :, :] = grad
+                # print(f"{batch_idx = }")
+                atomic_virial[batch_idx, :, :] = jac[batch_idx, n_batch, :, :]
+
+            # for n_batch in range(num_batch):
+            #     batch_i = data[AtomicDataDict.BATCH_PTR_KEY][n_batch]
+            #     batch_j = data[AtomicDataDict.BATCH_PTR_KEY][n_batch+1]
+            #     batch_idx = list(range(batch_i, batch_j))
+            #     print(f"{batch_idx = }")
+            #     jac = torch.autograd.functional.jacobian(
+            #         func=lambda s: batch_wrapper(s, n_batch, batch_idx),
+            #         inputs=data["_strain"][n_batch, :, :],
+            #         create_graph=self.training,  # needed to allow gradients of this output during training
+            #         # vectorize=self.vectorize,
+            #         # strategy="forward-mode",
+            #     )
+            #     atomic_virial[batch_idx, :, :] = jac
+            #     print(f"{jac = }")
+            # print(f"{num_batch = }")
 
 
         else:
-            grad = torch.autograd.functional.jacobian(
+            jac = torch.autograd.functional.jacobian(
                 func=wrapper,
                 inputs=data["_strain"],
                 # create_graph=self.training,  # needed to allow gradients of this output during training
                 vectorize=self.vectorize,
                 strategy="forward-mode",
             )
-            atomic_virial = grad
+            atomic_virial = jac
+            # print(f"{jac = }")
+            # print(f"{num_batch = }")
+
+        # print(f"{atomic_virial.shape = }")
+        # print(f"{atomic_virial = }")
 
         # Store virial
         virial = scatter(atomic_virial, batch, dim=0, reduce="sum")